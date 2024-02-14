20cefc255448ba2f6a87cd5191374aa8104472a5
edymol <edymolinap@gmail.com>
2024-02-14 20:17:50 +0000

    add: exceptions handler and validations to menu

--- restaurant/src/main/java/com/restaurantmanagement/entity/Menu.java
+++ restaurant/src/main/java/com/restaurantmanagement/entity/Menu.java
@@ -1,7 +1,14 @@
 package com.restaurantmanagement.entity;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -13,6 +20,8 @@ import java.sql.Timestamp;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Menu implements Serializable {
 
     @Serial
@@ -24,20 +33,26 @@ public class Menu implements Serializable {
     private Long menuId;
 
     @Column(name = "name")
+    @NotBlank(message = "Name cannot be empty")
     private String name;
 
     @Column(name = "description")
+    @Size(min = 50, max = 250, message = "Description must be between 50 and 250 characters")
     private String description;
 
     @Column(name = "price")
+    @NotNull(message = "Price cannot be null")
     private BigDecimal price;
 
     @Column(name = "category")
+    @NotBlank(message = "Category cannot be empty")
     private String category;
 
     @Column(name = "image_url")
     private String imageURL;
 
+    @Future(message = "Date must be in the future")
+    @NotNull(message = "Date cannot be null")
     private Date date;
 
     @Column(name = "created_at", nullable = false, updatable = false)
